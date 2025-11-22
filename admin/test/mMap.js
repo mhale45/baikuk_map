e6d62ede9d238ece81d7cb47bf71e3e6c0fcd921
mhale45 <ymg1633@gmail.com>
2025-11-23 02:15:47 +0900

    수동 업데이트

--- admin/test/mMap.js
+++ admin/test/mMap.js
@@ -246,11 +246,7 @@ async function renderListingsOnMap() {
                     const status = i.transaction_status || "";
 
                     // 🔥 상태에 따른 아이콘 선택
-                    const icon = status.includes("완료")
-                        ? "🔹"
-                        : status.includes("보류")
-                            ? "◆"
-                            : "🔸";
+                    const icon = status.includes("완료") ? "🔹" : "🔸";
 
                     const textColor = (() => {
                         if (status.includes("완료")) return "red";
