caa9a75c59186cd9b140435304bd311d1657a3e3
mhale45 <ymg1633@gmail.com>
2025-11-22 20:42:46 +0900

    수동 업데이트

--- admin/mMap/mMap.js
+++ admin/mMap/mMap.js
@@ -113,7 +113,7 @@ async function renderListingsOnMap() {
                 overflow-wrap: break-word;
                 max-width: 240px;
                 text-indent: -12px;
-                padding-left: 12px; ">${htmlLines.join("<br/>")}</div>`;
+                padding-left: 12px; "></div>`;
 
         const info = new kakao.maps.InfoWindow({
             content: infoHtml
