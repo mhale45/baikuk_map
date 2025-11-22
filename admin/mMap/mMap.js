f6af70fe3303ca8b3012658743cc851213ccfc88
mhale45 <ymg1633@gmail.com>
2025-11-22 20:47:49 +0900

    수동 업데이트

--- admin/mMap/mMap.js
+++ admin/mMap/mMap.js
@@ -99,8 +99,8 @@ async function renderListingsOnMap() {
                     white-space: normal;
                     word-wrap: break-word;
                     overflow-wrap: break-word;
-                ">🔹 ${i.listing_id} ${i.listing_title || "-"} 
-                    ${i.deposit_price || "-"} / ${i.monthly_rent || "-"} 
+                ">🔹 ${i.listing_id} ${i.listing_title || "-"} <br/>
+                    &nbsp;&nbsp;${i.deposit_price || "-"} / ${i.monthly_rent || "-"} 
                     - ${i.area_py || "-"} </div>
             `;
         });
