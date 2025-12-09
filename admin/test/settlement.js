2a5b98fbf190acb94dc012d336015b19eee8ac21
mhale45 <ymg1633@gmail.com>
2025-12-09 22:19:39 +0900

    수동 업데이트

--- admin/test/settlement.js
+++ admin/test/settlement.js
@@ -472,8 +472,8 @@ function renderMonthlyTable({ titleAffiliation, salesMap, payrollByStaff, costMa
       <td class="border px-2 py-2 text-right font-semibold text-red-600">${fmt(payrollTotal)}</td>
       <td class="border px-2 py-2 text-right">${fmt(__LAST_TAX_INVOICE_MAP[ym] || 0)}</td>
       <td class="border px-2 py-2 text-right ">${fmt(expectedVat)}</td>
-      <td class="border px-2 py-2 text-right font-semibold text-red-600">${fmt(vat)}</td>
-      <td class="border px-2 py-2 text-right">${fmt(realVat)}</td>
+      <td class="border px-2 py-2 text-right">${fmt(vat)}</td>
+      <td class="border px-2 py-2 text-right font-semibold text-red-600">${fmt(realVat)}</td>
       <td class="border px-2 py-2 text-right">${fmt(reserve)}</td>
       <td class="border px-2 py-2 text-right font-semibold text-green-600">${fmt(netIncome)}</td>
       <td class="border px-2 py-2 text-right font-semibold text-red-600">${fmt(totalCost)}</td>
