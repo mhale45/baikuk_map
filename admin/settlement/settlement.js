2db07d397ee3b959f515634ace544a1231d7c186
mhale45 <ymg1633@gmail.com>
2025-12-09 21:44:15 +0900

    수동 업데이트

--- admin/settlement/settlement.js
+++ admin/settlement/settlement.js
@@ -468,12 +468,12 @@ function renderMonthlyTable({ titleAffiliation, salesMap, payrollByStaff, costMa
       <td class="border px-2 py-2 text-right">${fmt(subBal)}</td>
       <td class="border px-2 py-2 text-right font-semibold">${fmt(payrollTotal)}</td>
       <td class="border px-2 py-2 text-right">${fmt(__LAST_TAX_INVOICE_MAP[ym] || 0)}</td>
-      <td class="border px-2 py-2 text-right font-semibold">${fmt(expectedVat)}</td>
+      <td class="border px-2 py-2 text-right font-semibold text-red-600">${fmt(expectedVat)}</td>
       <td class="border px-2 py-2 text-right">${fmt(vat)}</td>
       <td class="border px-2 py-2 text-right font-semibold">${fmt(reserve)}</td>
       <td class="border px-2 py-2 text-right font-semibold">${fmt(netIncome)}</td>
-      <td class="border px-2 py-2 text-right font-semibold text-blue-600">${fmt(totalCost)}</td>
-      <td class="border px-2 py-2 text-right text-purple-700">${fmt(dispAutonomousFee)}</td>
+      <td class="border px-2 py-2 text-right font-semibold text-red-600">${fmt(totalCost)}</td>
+      <td class="border px-2 py-2 text-right font-semibold text-purple-700">${fmt(dispAutonomousFee)}</td>
       <td class="border px-2 py-2 text-right font-semibold text-amber-700">${fmt(dispFinalProfit)}</td>
     `;
 
