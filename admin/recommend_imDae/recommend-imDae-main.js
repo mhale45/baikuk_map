bfc1325fc18de19ed73b1842809c37781a9b619f
mhale45 <ymg1633@gmail.com>
2026-04-21 23:14:16 +0900

    수동 업데이트

--- admin/recommend_imDae/recommend-imDae-main.js
+++ admin/recommend_imDae/recommend-imDae-main.js
@@ -658,8 +658,8 @@ async function loadCurrentUserStaffInfo() {
           mainLogo.src = 'https://sfinbtiqlfnaaarziixu.supabase.co/storage/v1/object/public/biakuk-images//starfield-logo.png';
           mainLogo.style.width = '10rem';
         } else if (aff === '1등운정점') {
-          mainLogo.src = 'https://sfinbtiqlfnaaarziixu.supabase.co/storage/v1/object/public/biakuk-images//1st_simbol+name.png';
-          mainLogo.style.width = '18rem';
+          mainLogo.src = 'https://sfinbtiqlfnaaarziixu.supabase.co/storage/v1/object/public/biakuk-images//1st_simbol+name_12.png';
+          mainLogo.style.width = '17rem';
         } else {
           mainLogo.src = 'https://sfinbtiqlfnaaarziixu.supabase.co/storage/v1/object/public/biakuk-images//baikuk-logo-yellow_simbol_name.png';
           mainLogo.style.width = '22rem';
