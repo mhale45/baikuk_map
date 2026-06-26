13d0b76ac8a2b1ee100f630863dd05391cc8c514
mhale45 <ymg1633@gmail.com>
2026-06-26 23:43:24 +0900

    수동 업데이트

--- admin/ad_management/ad_management.js
+++ admin/ad_management/ad_management.js
@@ -582,7 +582,12 @@ async function loadRenewalResults(channel, idVal) {
         <tr class="border-b hover:bg-gray-50 transition-colors">
           <td class="px-4 py-3 text-xs text-gray-500 font-mono">${dateStr}</td>
           <td class="px-4 py-3">${statusBadge}</td>
-          <td class="px-4 py-3 text-gray-600 font-mono">${item.naver_listing_id || '-'}</td>
+          <td class="px-4 py-3 text-gray-600 font-mono">
+            ${item.naver_listing_id 
+              ? `<a href="https://agency.neonet.co.kr/novo-agency/view/offerings/NaverOfferingsList.neo?search_type=total&search_text=${item.naver_listing_id}" target="_blank" class="text-blue-600 hover:underline font-semibold">${item.naver_listing_id}</a>` 
+              : '-'
+            }
+          </td>
           <td class="px-4 py-3 text-gray-600 font-mono">
             ${item.listing_id 
               ? `<a href="https://baikuk.com/item/view/${item.listing_id}" target="_blank" class="text-blue-600 hover:underline font-semibold">${item.listing_id}</a>` 
