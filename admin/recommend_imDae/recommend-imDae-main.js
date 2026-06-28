e9832c8b40901407f3563c1722e24b542a031c97
mhale45 <ymg1633@gmail.com>
2026-06-29 00:51:21 +0900

    수동 업데이트

--- admin/recommend_imDae/recommend-imDae-main.js
+++ admin/recommend_imDae/recommend-imDae-main.js
@@ -661,11 +661,11 @@ async function loadCurrentUserStaffInfo() {
         } else if (aff === '1등운정점') {
           mainLogo.src = 'https://sfinbtiqlfnaaarziixu.supabase.co/storage/v1/object/public/biakuk-images//1st_simbol+name_12.png';
           mainLogo.style.width = '17rem';
-          mainLogo.style.marginTop = '1.1rem';
+          mainLogo.style.marginTop = '1.2rem';
         } else {
           mainLogo.src = 'https://sfinbtiqlfnaaarziixu.supabase.co/storage/v1/object/public/biakuk-images//baikuk-logo-yellow_simbol_name.png';
           mainLogo.style.width = '22rem';
-          mainLogo.style.marginTop = '1.35rem';
+          mainLogo.style.marginTop = '1.4rem';
         }
       }
     }
