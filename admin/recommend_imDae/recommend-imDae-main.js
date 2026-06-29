7d4774ecd90c6dfa4b6f041c0b5955e3f26008eb
mhale45 <ymg1633@gmail.com>
2026-06-29 16:19:15 +0900

    수동 업데이트

--- admin/recommend_imDae/recommend-imDae-main.js
+++ admin/recommend_imDae/recommend-imDae-main.js
@@ -1394,7 +1394,7 @@ function updateListingsTableByInputs() {
         <span contenteditable="false" spellcheck="false" class="text-base flex items-center justify-center w-full h-full min-h-[2.5rem] cursor-text rounded hover:bg-gray-100 px-1 transition-all" data-field="area_py_${i}"></span>
       </td>
       <td class="border p-1 text-center align-middle hover:bg-gray-100 focus-within:bg-white focus-within:border focus-within:border-blue-500 transition-colors rounded" style="height: inherit;">
-        <textarea class="w-full h-auto min-h-[1.5rem] box-border text-center text-base border-none bg-transparent outline-none resize-none leading-tight overflow-hidden" data-field="description_${i}"></textarea>
+        <textarea rows="1" class="w-full h-auto min-h-[1.5rem] box-border text-center text-base border-none bg-transparent outline-none resize-none leading-tight overflow-hidden" data-field="description_${i}"></textarea>
       </td>
     `;
 
